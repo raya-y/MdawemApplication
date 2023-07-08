805b76b20411103a31239b808a1ac187372a8ea3
raya-y <127900567+raya-y@users.noreply.github.com>
2023-07-08 16:32:38 +0300

    Delete Attendance.py

--- Attendance.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Attendance review")
-st.write("Thanks for attanding today! you have been last seen at 04:34:22")
-st.write("You have a total attendance of 3 hours and 44 minutes so far today")
-
