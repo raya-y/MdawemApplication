daa518f95de5c491a9c99f5d50cb8ce95e297a1e
raya-y <127900567+raya-y@users.noreply.github.com>
2023-07-08 16:32:51 +0300

    Delete SC_Attendance.py

--- SC_Attendance.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("You have not been seen in the premises for more than 20m!!")
-st.write("If you have not exited the premises please upload a picture of you.")
-uploaded_file = st.file_uploader("")
