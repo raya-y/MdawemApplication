1660e16f659326e53b7428915df15595e3e45a99
raya-y <127900567+raya-y@users.noreply.github.com>
2023-07-08 16:30:57 +0300

    Add files via upload

---
+++ Mdawem.py/pages/SC_Attendance.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("You have not been seen in the premises for more than 20m!!")
+st.write("If you have not exited the premises please upload a picture of you.")
+uploaded_file = st.file_uploader("")
