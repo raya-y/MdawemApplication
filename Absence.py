8abac53cc862323cb298557a98a832b8cb686fb0
raya-y <127900567+raya-y@users.noreply.github.com>
2023-07-08 16:32:23 +0300

    Delete Absence.py

--- Absence.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Are you not attending work today?")
-st.write("Sorry about hearing that, we hope you are okay.")
-uploaded_file = st.file_uploader("Please attach your excuse here...")
