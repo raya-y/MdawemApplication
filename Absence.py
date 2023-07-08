8f5fe607f1c7dc1cd27694b43de9418ef27f8635
raya-y <127900567+raya-y@users.noreply.github.com>
2023-07-08 16:28:21 +0300

    Add files via upload

---
+++ Absence.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Are you not attending work today?")
+st.write("Sorry about hearing that, we hope you are okay.")
+uploaded_file = st.file_uploader("Please attach your excuse here...")
